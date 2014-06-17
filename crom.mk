5b1d0d8a4d0de9a851c4d3114ef6f97ab0a48166
aways <aways@c-rom.org>
2014-06-17 19:27:24 +0200

    Set it for new vendor.

--- crom.mk
+++ crom.mk
@@ -3,6 +3,10 @@
 
 PRODUCT_RELEASE_NAME := NS
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common C-RoM stuff.
 $(call inherit-product, vendor/crom/config/common_phone.mk)
 
@@ -10,17 +14,17 @@ $(call inherit-product, vendor/crom/config/common_phone.mk)
 $(call inherit-product, device/samsung/crespo/full_crespo.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+#set camera info
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.res=5MP
 
 PRODUCT_NAME := crom_crespo
 PRODUCT_DEVICE := crespo
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus S
 PRODUCT_MANUFACTURER := Samsung
-
-# media
-PRODUCT_COPY_FILES += \
-    vendor/crom/prebuilt/common/media/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT="google/soju/crespo:4.1.2/JZO54K/485486:user/release-keys" PRIVATE_BUILD_DESC="soju-user 4.1.2 JZO54K 485486 release-keys"
